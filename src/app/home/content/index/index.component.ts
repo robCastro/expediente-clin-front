b67819ba4a17b2d43810d0962f754daf08c95868
brenNavas <32327822+brenNavas@users.noreply.github.com>
2019-06-27 09:06:42 -0600

    bienvenido

--- src/app/home/content/index/index.component.ts
+++ src/app/home/content/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/usuarios/auth.service';
 
 @Component({
   selector: 'app-index',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router:Router) { }
 
   ngOnInit() {
   }
